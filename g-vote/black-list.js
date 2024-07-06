fa7f8a6a22964602e9f55ad3d6bd88ab8742b44a
Nudnuy00 <34570152+Nudnuy00@users.noreply.github.com>
2024-07-06 23:51:19 +0300

    Add files via upload

---
+++ g-vote/black-list.js
@@ -0,0 +1,28 @@
+let BL = [
+"Call of Duty: Warzone",
+"Cult Of The Lamb ",
+"Dead Space 3",
+"Genshin Impact",
+"Kena Bridge of Spirits",
+"Minecraft: Story Mode",
+"Mirror's Edge Catalyst",
+"Outlast 1",
+"Resident Evil Village",
+"South Park: Fractured but Whole",
+"The Beast Inside",
+"The Evil Within",
+"The Last Of Us 2",
+"The Outlast Trials",
+"Trove",
+];
+
+
+
+
+
+
+
+
+
+
+
---
+++ g-vote/gListData.js
@@ -0,0 +1,20 @@
+let GL = [
+{
+name: 'RimWorld',
+voteNow: 5,
+voteNeed: 12,
+alwaysOnTop: 1,
+description: 'одиночная, симулятор колонии, вид сверху, примитивная рисовка, футуризм, выживание',
+imgSRC: '../img/RimWorld.avif',
+occupancy: 0,
+},
+{
+name: 'VALORANT',
+voteNow: 3,
+voteNeed: 4,
+alwaysOnTop: 0,
+description: 'командный туктический шутер от первого лица',
+imgSRC: '../img/VALORANT.avif',
+occupancy: 0,
+},
+];
